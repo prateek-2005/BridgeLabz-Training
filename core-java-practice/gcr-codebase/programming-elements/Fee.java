3966cbc59920edcdc15bbdef6f9dea370b101762
prateek-2005 <0000psyp@gmail.com>
2025-12-18 12:56:17 +0530

    [Prateek] added a new file named Fee.java under gcr-codebase programming-elements

---
+++ core-java-practice/gcr-codebase/programming-elements/Fee.java
@@ -0,0 +1,9 @@
+public class Fee {
+    public static void main(String[] args) {
+        int fee=125000;
+        int discountPercent=10;
+        int discount=((discountPercent/100)*fee);
+        int pay=fee-discount;
+        System.out.println("The discount amount is INR "+discount+" and final discounted fee is INR "+pay);
+    }
+}
