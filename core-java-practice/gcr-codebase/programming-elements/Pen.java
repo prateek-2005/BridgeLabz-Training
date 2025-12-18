619f951f5b6fa530407d61672ce69f2493066113
prateek-2005 <0000psyp@gmail.com>
2025-12-18 12:44:28 +0530

    [Prateek] added a new file named Pen.java under gcr-codebase programming-elements

---
+++ core-java-practice/gcr-codebase/programming-elements/Pen.java
@@ -0,0 +1,9 @@
+public class Pen {
+    public static void main(String[] args) {
+        int total=14;
+        int person=3;
+        int remaining=14%3;
+        int penperperson=(int)14/3;
+        System.out.println("The pen per student is "+penperperson+" and the remaining pen not distributed is "+remaining);
+    }
+}
