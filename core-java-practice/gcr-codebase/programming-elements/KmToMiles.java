7acbc75059e09bb518245946f4bc5c8b156c4382
prateek-2005 <0000psyp@gmail.com>
2025-12-18 11:51:40 +0530

    [prateek] added a new file named KmToMiles.java under gcr-codebase programming-elements

---
+++ core-java-practice/gcr-codebase/programming-elements/KmToMiles.java
@@ -0,0 +1,7 @@
+public class KmToMiles{
+    public static void main(String[] args) {
+        double km=10.8;
+        double miles=((double) 10.8*1.6);
+        System.out.println("The distance "+km +"in miles is "+miles);
+    }
+}
