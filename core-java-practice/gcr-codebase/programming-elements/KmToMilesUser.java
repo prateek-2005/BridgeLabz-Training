7b33816b305d2abd0db7ec5dbe9eb11a1e4ec992
prateek-2005 <0000psyp@gmail.com>
2025-12-18 16:36:09 +0530

    [Prateek] added a new file named KmToMilesUser.java under gcr-codebase programming-elements

--- core-java-practice/gcr-codebase/programming-elements/KmToMilesUser.java
+++ core-java-practice/gcr-codebase/programming-elements/KmToMilesUser.java
@@ -3,7 +3,7 @@ public class KmToMilesUser {
     public static void main(String[] args) {
         Scanner sc=new Scanner(System.in);
         double km=sc.nextDouble();
-        double miles=((double) km*1.6);
+        double miles=((double) km/1.6);
         System.out.println("The total miles is "+miles+" mile for the given "+km+"km");
     }
 }
